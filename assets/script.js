f1d062c969d9c7890a16883f5a0c3315f2cdf4f4
amatos <amatos201@gmail.com>
2022-10-12 01:37:39 -0400

    add script

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var citiesListArr = [];
+var numOfCities = 5;
+var APIKey = 
+
+var WeatherApi =
+  "https://api.openweathermap.org/data/2.5/weather?q=";
+var dailyApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
+var forecastWeatherApiStarts =
+  "https://api.openweathermap.org/data/2.5/onecall?";
